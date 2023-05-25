5b105d71a5699b91a0f3c0baec3e8bdc249e7339
Masasori <127400583+Masasori@users.noreply.github.com>
2023-05-25 22:02:35 +0200

    Update MASA_YEPItemSlotsExParams.js

--- MASA_YEPItemSlotsExParams.js
+++ MASA_YEPItemSlotsExParams.js
@@ -54,46 +54,50 @@ if (Imported.YEP_X_ItemUpgrades) {
 Masa.ItemManager_effectIUSParamRateChange = ItemManager.effectIUSParamRateChange;
 ItemManager.effectIUSParamRateChange = function(item, stat, value) {
     Masa.ItemManager_effectIUSParamRateChange.call(this);
+    algSign = "";
+    if (value > 0){
+        algSign = "+";
+    }
     switch (stat) {
       case 'EX1':
         item.traits.push({code: 22, dataId: 0, value: value/100});
-        item.description += " (Hit Rate +" + value + "%)";
+        item.description += " (Hit Rate " + algSign + value + "%)";
         break;
       case 'EX2':
         item.traits.push({code: 22, dataId: 1, value: value/100});
-        item.description += " (Evasion Rate +" + value + "%)";
+        item.description += " (Evasion Rate " + algSign + value + "%)";
         break;
       case 'EX3':
         item.traits.push({code: 22, dataId: 2, value: value/100});
-        item.description += " (Critical Rate +" + value + "%)";
+        item.description += " (Critical Rate " + algSign + value + "%)";
         break;
       case 'EX4':
         item.traits.push({code: 22, dataId: 3, value: value/100});
-        item.description += " (Critical Evasion +" + value + "%)";
+        item.description += " (Critical Evasion " + algSign + value + "%)";
         break;
       case 'EX5':
         item.traits.push({code: 22, dataId: 4, value: value/100});
-        item.description += " (Magic Evasion +" + value + "%)";
+        item.description += " (Magic Evasion " + algSign + value + "%)";
         break;
       case 'EX6':
         item.traits.push({code: 22, dataId: 5, value: value/100});
-        item.description += " (Magic Reflection +" + value + "%)";
+        item.description += " (Magic Reflection " + algSign + value + "%)";
         break;
       case 'EX7':
         item.traits.push({code: 22, dataId: 6, value: value/100});
-        item.description += " (Counter Attack +" + value + "%)";
+        item.description += " (Counter Attack " + algSign + value + "%)";
         break;
       case 'EX8':
         item.traits.push({code: 22, dataId: 7, value: value/100});
-        item.description += " (HP Regeneration +" + value + "%)";
+        item.description += " (HP Regeneration " + algSign + value + "%)";
         break;
       case 'EX9':
         item.traits.push({code: 22, dataId: 8, value: value/100});
-        item.description += " (MP Regeneration +" + value + "%)";
+        item.description += " (MP Regeneration " + algSign + value + "%)";
         break;
       case 'EX10':
         item.traits.push({code: 22, dataId: 9, value: value/100});
-        item.description += " (TP Regeneration +" + value + "%)";
+        item.description += " (TP Regeneration " + algSign + value + "%)";
         break;
     }
 };
