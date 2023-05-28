4b01cd78c2836bd2c162b65eb2ec3caca9bcea7f
Masasori <127400583+Masasori@users.noreply.github.com>
2023-05-28 13:58:03 +0200

    Update MASA_Extension_YEPItemSlots_ExParams.js
    Version 1.00

--- MASA_Extension_YEPItemSlots_ExParams.js
+++ MASA_Extension_YEPItemSlots_ExParams.js
@@ -1,12 +1,12 @@
 //=============================================================================
-// Masa_Extension_YEPItemSlots_ExParams.js
+// Masa_Extension_YEPItemSlots_Traits.js
 //=============================================================================
 
 var Imported = Imported || {};
-Imported.Masa_Extension_YEPItemSlots_ExParams = true;
+Imported.Masa_Extension_YEPItemSlots_Traits = true;
 
 var Masa = Masa || {};
-Masa.Extension_YEPItemSlots_ExParams = Masa.Extension_YEPItemSlots_ExParams || {};
+Masa.Extension_YEPItemSlots_Traits = Masa.Extension_YEPItemSlots_Traits || {};
 
 //=============================================================================
  /*:
@@ -19,8 +19,8 @@ Masa.Extension_YEPItemSlots_ExParams = Masa.Extension_YEPItemSlots_ExParams || {
  * Introduction
  * ============================================================================
  *
- * This plugin expands YEP_X_ItemUpgradeSlots Plugin
- * with the function to add Ex-Paramters.
+ * This plugin expands YEP_X_ItemUpgradeSlots Plugins
+ * with the function to add Traits.
  *
  * ============================================================================
  * How to setup?
@@ -31,71 +31,141 @@ Masa.Extension_YEPItemSlots_ExParams = Masa.Extension_YEPItemSlots_ExParams || {
  * How to use it?
  * =============
  * Simply use notetags as described in YEPs plugin.
- * To add Ex-Paramters use "EX" following the number
- * of the index (starting from 1) from the list of 
- * the editor:
+ * 
+ * Element Rates:
+ * 
+ * To add "Element Rates" write "ER: " than the index 
+ * of the element in the editor (starting with index 1)
+ * followed by the percentage.
+ *
+ * e.g: Element 1 80%
+ *   <Upgrade Effect>
+ *    ER: 1 80%
+ *   </Upgrade Effect>
+ *
+ * e.g: Element 3 120%
+ *   <Upgrade Effect>
+ *    ER: 3 120%
+ *   </Upgrade Effect>
+ *
+ *
+ * To add "Ex-Paramters" use the abbreviations below:
+ *
+ * HR = Hit rate
+ * ER = Evasion Rate
+ * CR = Critical Rate
+ * CE = Critical Evasion
+ * ME = Magic Evasion
+ * MR = Magic Reflection
+ * CA = Counter Attack
+ * HPR = HP Regeneration
+ * MPR = MP Regeneration
+ * TPR = TP Regeneration
  *
  * e.g: Hit Rate + 20%
  *   <Upgrade Effect>
- *    EX1: +20%
+ *    HR: +20%
  *   </Upgrade Effect>
  *
  * e.g: Evasion Rate - 5%
  *   <Upgrade Effect>
- *    EX2: -5%
+ *    ER: -5%
  *   </Upgrade Effect>
  *
+ *
+ * Attack Element:
+ * 
+ * To add "Attack Element" write "AE: " than the index 
+ * of the element in the editor (starting with index 1).
+ *
+ * e.g: Element 1
+ *   <Upgrade Effect>
+ *    AE: 1
+ *   </Upgrade Effect>
+ *
+ * e.g: Element 3
+ *   <Upgrade Effect>
+ *    AE: 3
+ *   </Upgrade Effect>
+ *
+ *
+ * ============================================================================
+ * Changelog
+ * ============================================================================
+ *
+ * Version 1.00:
+ * - Finished plugin!
+ * - Element Rates
+ * - Ex-Parameters
+ * - Attack Element
  */
 //=============================================================================
 
 if (Imported.YEP_X_ItemUpgrades) {
 
 //=============================================================================
-Masa.Extension_YEPItemSlots_ExParams.ItemManager_effectIUSParamRateChange = ItemManager.effectIUSParamRateChange;
+Masa.Extension_YEPItemSlots_Traits.ItemManager_processIUSEffect = ItemManager.processIUSEffect;
+ItemManager.processIUSEffect = function(line, mainItem, effectItem) {
+    Masa.Extension_YEPItemSlots_Traits.ItemManager_processIUSEffect.call(this, line, mainItem, effectItem);
+    // AE: X
+    if (line.match(/AE:[ ](\d+)/i)) {
+      var value = parseInt(RegExp.$1);
+      return this.addTraitToItem(mainItem, 31, value, 1);
+    }
+    // ER: X Y%
+    if (line.match(/ER:[ ](\d+)[ ](\d+)([%％])/i)) {
+      var value1 = parseInt(RegExp.$1);
+      var value2 = parseInt(RegExp.$2);
+      return this.addTraitToItem(mainItem, 11, value1, value2/100);
+    }
+};
+
+Masa.Extension_YEPItemSlots_Traits.ItemManager_effectIUSParamRateChange = ItemManager.effectIUSParamRateChange;
 ItemManager.effectIUSParamRateChange = function(item, stat, value) {
-    Masa.Extension_YEPItemSlots_ExParams.ItemManager_effectIUSParamRateChange.call(this);
+    Masa.Extension_YEPItemSlots_Traits.ItemManager_effectIUSParamRateChange.call(this, item, stat, value);
     algSign = "";
     if (value > 0){
         algSign = "+";
     }
     switch (stat) {
-      case 'EX1':
+      //Ex-Parameters
+      case 'HR':
         item.traits.push({code: 22, dataId: 0, value: value/100});
-        item.description += " (Hit Rate " + algSign + value + "%)";
+        item.description += " (Hit Rate " + algSign + value + "%)";	
         break;
-      case 'EX2':
+      case 'ER':
         item.traits.push({code: 22, dataId: 1, value: value/100});
         item.description += " (Evasion Rate " + algSign + value + "%)";
         break;
-      case 'EX3':
+      case 'CR':
         item.traits.push({code: 22, dataId: 2, value: value/100});
         item.description += " (Critical Rate " + algSign + value + "%)";
         break;
-      case 'EX4':
+      case 'CE':
         item.traits.push({code: 22, dataId: 3, value: value/100});
         item.description += " (Critical Evasion " + algSign + value + "%)";
         break;
-      case 'EX5':
+      case 'ME':
         item.traits.push({code: 22, dataId: 4, value: value/100});
         item.description += " (Magic Evasion " + algSign + value + "%)";
         break;
-      case 'EX6':
+      case 'MR':
         item.traits.push({code: 22, dataId: 5, value: value/100});
         item.description += " (Magic Reflection " + algSign + value + "%)";
         break;
-      case 'EX7':
+      case 'CA':
         item.traits.push({code: 22, dataId: 6, value: value/100});
         item.description += " (Counter Attack " + algSign + value + "%)";
         break;
-      case 'EX8':
+      case 'HPR':
         item.traits.push({code: 22, dataId: 7, value: value/100});
         item.description += " (HP Regeneration " + algSign + value + "%)";
         break;
-      case 'EX9':
+      case 'MPR':
         item.traits.push({code: 22, dataId: 8, value: value/100});
         item.description += " (MP Regeneration " + algSign + value + "%)";
         break;
-      case 'EX10':
+      case 'TMP':
         item.traits.push({code: 22, dataId: 9, value: value/100});
         item.description += " (TP Regeneration " + algSign + value + "%)";
         break;
